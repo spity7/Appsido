39a33d22e84c29c64a45c8e9c816179ee543ccdf
hasan sbaity <th.rider.clan@gmail.com>
2025-01-02 00:53:05 +0200

    starting the project

---
+++ src/components/layout/LayoutOne.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../home-one/footer";
+import Header from "../home-one/header";
+
+function LayoutOne() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default LayoutOne;
---
+++ src/hooks/useScrollPosition.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export default function useScrollPosition() {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 50);
+    });
+  }, []);
+
+  return scroll;
+}
